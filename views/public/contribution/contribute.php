c57a9823f0864eedf44834363639df34b4bb6e2c
patrickmj <patrickmjchnm@gmail.com>
2013-05-22 16:01:04 -0400

    First push up to my repo. @bckyferris should fork this over to her account, and make this the standard people work from.
    The precise naming conventions turned out didn't work well with the new repo name. Doing the fork should make things easier.

---
+++ views/public/contribution/contribute.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * @version $Id$
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ * @copyright Center for History and New Media, 2010
+ * @package Contribution
+ */
+
+queue_js_file('contribution-public-form');
+queue_js_file('jquery.bxslider.min');
+queue_css_file('jquery.bxslider');
+$contributionPath = get_option('contribution_page_path');
+if(!$contributionPath) {
+    $contributionPath = 'contribution';
+}
+queue_css_file('form');
+
+$head = array('title' => 'Contribute',
+              'bodyclass' => 'contribution');
+echo head($head); ?>
+<script type="text/javascript">
+// <![CDATA[
+jQuery(document).ready(function() {            
+enableContributionTypeButtons(<?php echo js_escape(url($contributionPath.'/type-form')); ?>);
+});
+// ]]>
+</script>
+
+<div id="primary">
+<?php echo flash(); ?>
+    
+    <h1><?php echo $head['title']; ?></h1>
+
+    
+        <!--  change this to tabs  steal from how admin/items/edit does it-->
+        <div>
+        <?php $options = get_table_options('ContributionType' ); ?>
+        <?php array_shift($options); ?>
+        <?php foreach($options as $id=>$option): ?>
+            <a href='' class='type-option' value='<?php echo $id+1; ?>'><?php echo $option; ?></a>
+        <?php endforeach; ?>
+        
+        </div>
+
+        <?php  if (isset($typeForm)): ?>
+        <?php //if (true): ?>
+        <form method="post" action="" enctype="multipart/form-data">
+            <div id="bx-pager">
+              <a data-slide-index="0" href="">Your Contribution</a>
+              <a data-slide-index="1" href="">Where and When</a>
+              <a data-slide-index="2" href="">Personal Information</a>
+              <a data-slide-index="3" href="">Share</a>
+              <input class="submit" type="submit" />
+            </div>
+            
+            <input type='hidden' name='contribution_type' value='1' />
+            <div id="form-container">
+            <ul class="bxslider">
+                
+                <li>
+                    <div class="form">
+                        <p>Your Contribution</p>
+                        <div id='type-form'>
+                            <?php echo $typeForm; ?>
+                        </div>
+                    </div>
+                </li>
+                
+
+                <li>
+                    <div class="form">
+                        <p>Where and When</p>
+                            <?php echo get_specific_plugin_hook_output('Geolocation', 'contribution_type_form', array('view' => $this, 'item' => $item, 'type'=>$type) ); ?>
+                            <!-- figure out how the calendar will work :) , and do the same thing!   -->
+                    </div>
+
+                </li>
+                                
+                
+                <li>
+                    <div class="form">
+                        <p>Personal Information</p>
+                            <?php echo $profileForm; ?>
+                            <!-- profile information here -->
+                    </div>
+                </li>
+                
+                <li>
+                    <div class="form">
+                        <p>Share</p>
+                        <fieldset id="contribution-confirm-submit" <?php if (!isset($typeForm)) { echo 'style="display: none;"'; }?>>
+                            <div class="inputs">
+                                <?php $public = isset($_POST['contribution-public']) ? $_POST['contribution-public'] : 0; ?>
+                                <?php echo $this->formCheckbox('contribution-public', $public, null, array('1', '0')); ?>
+                                <?php echo $this->formLabel('contribution-public', 'Publish my contribution on the web.'); ?>
+                            </div>
+                            <div class="inputs">
+                                <?php $anonymous = isset($_POST['contribution-anonymous']) ? $_POST['contribution-anonymous'] : 0; ?>
+                                <?php echo $this->formCheckbox('contribution-anonymous', $anonymous, null, array(1, 0)); ?>
+                                <?php echo $this->formLabel('contribution-anonymous', "Contribute anonymously."); ?>
+                            </div>
+                            <p>In order to contribute, you must read and agree to the <a href="<?php echo contribution_contribute_url('terms') ?>" target="_blank">Terms and Conditions.</a></p>
+                            <div class="inputs">
+                                <?php $agree = isset( $_POST['terms-agree']) ?  $_POST['terms-agree'] : 0 ?>
+                                <?php echo $this->formCheckbox('terms-agree', $agree, null, array('1', '0')); ?>
+                                <?php echo $this->formLabel('terms-agree', 'I agree to the Terms and Conditions.'); ?>
+                            </div>
+                        </fieldset>
+                    <?php echo $this->formSubmit('form-submit', 'Contribute', array('class' => 'submitinput')); ?>                           
+                    </div>
+                </li>
+            </ul>
+            </div>
+        </form>
+        
+<script>
+jQuery('.bxslider').bxSlider({
+  infiniteLoop: false,
+  hideControlOnEnd: true,
+  pagerCustom: '#bx-pager'
+});
+</script>
+        <?php endif; ?>
+</div>
+<?php echo foot();
---
+++ views/public/contribution/type-form.php
@@ -0,0 +1,60 @@
+
+<?php if (!$type): ?>
+<p>You must choose a contribution type to continue.</p>
+<?php else: ?>
+<h2>Contribute a <?php echo $type->display_name; ?></h2>
+
+<?php 
+if ($type->isFileRequired()):
+    $required = true;
+endif;
+?>
+
+
+
+<div class="field">
+        <?php echo $this->formLabel('contributed_file', 'Upload a file'); ?>
+        <?php echo $this->formFile('contributed_file', array('class' => 'fileinput')); ?>
+</div>
+
+<?php endif; ?>
+
+<?php 
+
+
+$this->profileForm = '';
+//put the prompts used for profile information in this array
+$profilePrompts = array(
+        'Title'
+        
+        
+        );
+?>
+
+<?php 
+foreach ($type->getTypeElements() as $contributionTypeElement) {
+    if(in_array( $contributionTypeElement->prompt, $profilePrompts)) {
+        $this->profileForm .= $this->elementForm($contributionTypeElement->Element, $item, array('contributionTypeElement'=>$contributionTypeElement));
+    } else {
+        echo $this->elementForm($contributionTypeElement->Element, $item, array('contributionTypeElement'=>$contributionTypeElement));        
+    }
+}
+?>
+
+<?php 
+if (!isset($required) && $type->isFileAllowed()):
+?>
+
+<div id="file" class="field">
+        <?php echo $this->formLabel('contributed_file', 'Upload a file (Optional)'); ?>
+        <?php echo $this->formFile('contributed_file', array('class' => 'fileinput')); ?>
+</div>
+<?php endif; ?>
+
+<?php $user = current_user(); ?>
+<?php if(get_option('contribution_simple') && !current_user()) : ?>
+<div class="field">
+    <?php echo $this->formLabel('contribution_simple_email', 'Email (Required)'); ?>
+    <?php echo $this->formText('contribution_simple_email'); ?>
+</div>
+<?php endif; ?>
